5a6090bfa75d6393ab3e8be2bf0088e52296f3b1
Baby Cruz <vulpine444@gmail.com>
2025-07-24 19:12:07 -0700

    Disable react-hooks/exhaustive-deps rule

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -15,7 +15,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
-      "react-hooks/exhaustive-deps": "warn",
+      "react-hooks/exhaustive-deps": "off",
       "react/no-unescaped-entities": "off",
       "@next/next/no-img-element": "off"
     }
