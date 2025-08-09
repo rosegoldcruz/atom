5a59825bb94190fb8ec73d7cc24869adedd225f7
Baby Cruz <vulpine444@gmail.com>
2025-08-09 12:27:17 -0700

    new pics

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import AtomHero3DBlack from './components/AtomHero3DBlack'
+import AitechSections from './components/AitechSections'
 
 export default function Page(){
   return (
     <main className="bg-black min-h-screen">
       <AtomHero3DBlack />
-      {/* existing sections below */}
+      <AitechSections />
     </main>
   )
 }
